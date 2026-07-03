0174dfb0ced9c6ee5c1bd268003e059aa26891fd
Roberto-Michael-Quintana-Rojas <farmedos402@gmail.com>
2026-07-03 17:52:53 -0400

    creacion del componente hero y tambien de informacion de las cards

--- examenFinal/src/App.tsx
+++ examenFinal/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Bienvenido from './components/Bienvenido'
 import Header from './components/Header'
+import Hero from './components/Hero'
+import InfoCards from './components/InfoCards'
 function App() {
   return (
     <div className="app">
       <Header />
+      <Hero />
+      <InfoCards />
       <Bienvenido />
 
     </div>
