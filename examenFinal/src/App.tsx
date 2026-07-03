3c995c1445255588f8f3e98be91a9c64233eee68
Roberto-Michael-Quintana-Rojas <farmedos402@gmail.com>
2026-07-03 17:30:16 -0400

    realizando el componente bienvenido

--- examenFinal/src/App.tsx
+++ examenFinal/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Bienvenido from './components/Bienvenido'
 
 function App() {
   return (
-    <>
-    </>
+    <div className="app">
+      <Bienvenido />
+
+    </div>
   )
 }
 
